f8fa9d9227f3fd052e713f52137c85ec3f2b3f1a
Rohigithubt <rohitjain68134@gmail.com>
2023-02-24 21:31:28 +0530

    diamond pattern

---
+++ diamond_star_pattern.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,k,n;
+    cout<<"enter the number=";
+    cin>>n;
+    for(i=1;i<n;i++)
+    {
+        for(k=0;k<=n-i;k++)
+        {
+            cout<<" ";
+        }
+        for(j=0;j<i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(i=0;i<n;i++)
+    {
+        for(k=0;k<i;k++)
+        {
+            cout<<" ";
+        }
+        for(j=0;j<n-i;j++)
+        {
+            cout<<" *";
+        }
+        cout<<endl;
+    }
+}
+output->
+
+enter the number=5
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * *
+  * * * *
+   * * *
+    * *
+     *
