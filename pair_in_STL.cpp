e22ff704dd1ac2dcda7980afec8f18e1ef3cc491
Rohigithubt <rohitjain68134@gmail.com>
2023-03-03 22:29:01 +0530

    pair basic program in c++

---
+++ pair_in_STL.cpp
@@ -0,0 +1,11 @@
+//pair in STL
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<int,string> p={1,"abc"};
+    cout<<p.first<<" "<<p.second<<endl;    
+}
+   /*output->
+
+1 abc   */
