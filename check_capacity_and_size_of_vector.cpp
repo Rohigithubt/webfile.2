774d2c8d0d4c61feaca4b016920f1dc0f15926c5
Rohigithubt <rohitjain68134@gmail.com>
2023-03-16 22:11:54 +0530

    check capacity and size of vector

---
+++ check_capacity_and_size_of_vector.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    v.push_back(2);
+    v.push_back(4);
+    v.push_back(5);
+    cout<<"capacity="<<v.capacity()<<endl;
+    cout<<"size="<<v.size();
+    
+}
+/*  output->
+
+capacity=4
+size=3 */
